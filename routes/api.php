24c909780efe2e5d718c38a85c94b512235fc63b
konokyasin <yasinarafatkonok@gmail.com>
2020-07-13 10:57:59 +0600

    Setting up api route,model

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiresource('/products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+    Route::apiresource('/{product}/reviews', 'ReviewController');
+});
