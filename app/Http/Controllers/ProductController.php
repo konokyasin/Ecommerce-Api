ae80d4223237e5704926106fb431d4e9ce1b82d2
konokyasin <yasinarafatkonok@gmail.com>
2020-07-13 21:22:05 +0600

    delete product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -103,6 +103,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response(null, 204); 
     }
 }
