c6d40c77359a7eb661d7a685c39dba321a765b4c
konokyasin <yasinarafatkonok@gmail.com>
2020-07-13 21:08:01 +0600

    update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -86,7 +86,13 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+
+        return response([
+            'data' => new ProductResource($product)
+        ], 201); 
     }
 
     /**
