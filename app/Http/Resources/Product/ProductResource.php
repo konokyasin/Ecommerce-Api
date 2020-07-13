c43eca721806102aaa014793d4ece8ededdc8210
konokyasin <yasinarafatkonok@gmail.com>
2020-07-13 15:39:42 +0600

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends Resource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'discount_price' => round(( 1- ($this->discount)/100) * $this->price),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/
+            $this->reviews->count(),2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
