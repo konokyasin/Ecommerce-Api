562be5570d1b24d5f5573b391299f33e2eb3d14c
konokyasin <yasinarafatkonok@gmail.com>
2020-07-14 10:27:17 +0600

    Check product belongs to user or not.

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -8,6 +8,9 @@ $factory->define(App\Model\Product::class, function (Faker $faker) {
         'detail' => $faker->paragraph,
         'price' => $faker->numberBetween(100, 1000),
         'stock' => $faker->randomDigit,
-        'discount' => $faker->numberBetween(5, 30)
+        'discount' => $faker->numberBetween(5, 30),
+        'user_id' => function() {
+           return App\User::all()->random();
+        }
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Model\Product;
 use App\Model\Review;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(User::class, 5)->create();
         factory(Product::class, 50)->create();
         factory(Review::class, 300)->create();
     }
